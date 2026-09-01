8b2aa253537498cf73db1b6583ef426d098f7588
Joao Pedro <joaopedrommn97@gmail.com>
2026-09-01 03:00:18 -0300

    feat: implementa herança classe pessoa

--- Conta.cs
+++ Conta.cs
@@ -8,31 +8,47 @@ namespace SCBank
             Gerente = gerente;
             Numero = new Random().Next();
         }
-        public Cliente Titular { get; set; }
-        public Funcionario Gerente { get; set; }
-        
-        public int Numero { get; set; }
-        
+
+        public Cliente Titular { get; }
+        public Funcionario Gerente { get; }
+        public int Numero { get; }
+
         private decimal saldo;
 
         private List<Transacao> transacoes = new List<Transacao>();
-        
+
         public void Depositar(decimal valor)
         {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor do depósito deve ser maior que zero.");
+                return;
+            }
+
             transacoes.Add(new Transacao(valor, "Depósito"));
             saldo += valor;
         }
+
         public void Sacar(decimal valor)
         {
+            if (valor <= 0)
+            {
+                Console.WriteLine("O valor do saque deve ser maior que zero.");
+                return;
+            }
+
             if (valor > saldo)
             {
                 Console.WriteLine("Saldo insuficiente para saque.");
+                return;
             }
+
             Transacao saque = new Transacao(valor, "Saque");
             transacoes.Add(saque);
             saldo -= valor;
         }
-            public decimal ObterSaldo()
+
+        public decimal ObterSaldo()
         {
             return saldo;
         }
@@ -40,22 +56,28 @@ namespace SCBank
         public void ImprimirTransacoes()
         {
             Console.WriteLine("Transações:");
+
             foreach (var transacaoObj in transacoes)
             {
                 transacaoObj.ImprimirTransacao();
             }
         }
+
         public void ImprimirExtrato()
         {
             Titular.Imprimir();
+
             Console.WriteLine("======================");
             Console.WriteLine("Extrato da conta:");
             Console.WriteLine("======================");
+
             Console.WriteLine("Número da conta: " + Numero);
             Console.WriteLine("Titular: " + Titular.Nome);
             Console.WriteLine("CPF: " + Titular.Cpf);
             Console.WriteLine($"Saldo atual: {saldo:C2}");
+
             ImprimirTransacoes();
+
             Console.WriteLine("======================");
         }
     }
